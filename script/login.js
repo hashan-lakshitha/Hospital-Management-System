de010996c0c3124cdee31753847efb30b44e4e94
hashan-lakshitha <r123tharaka74@gmail.com>
2023-09-20 09:52:14 +0530

    init

---
+++ script/login.js
@@ -0,0 +1,6 @@
+const creatAnAccount=()=>{
+    window.location.replace('register.html');
+}
+const login=()=>{
+    window.location.replace('dashboard.html');
+}
---
+++ script/register.js
@@ -0,0 +1,3 @@
+const alreadyHaveAnAccount=()=>{
+    window.location.replace('login.html');
+}
